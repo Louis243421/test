bf194fced57a520ccaa7d5d0f7132d4edd056f9a
Luigi Improta <luigi@macbookLuigi.local>
2024-06-13 14:17:20 +0200

    gino

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gino's Studios</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    
+</head>
+<body>
+    <?php include 'menu.html'; ?>
+    <div class="content">
+        <div class="img"></div>
+        <div class="center">
+            <div class="title"></div>
+            <div class="sub_title"></div>
+            <div class="btns">
+                <button>Learn More</button>
+                <button>Subscribe</button>
+            </div>
+        </div>
+    </div>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
