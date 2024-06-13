fe474abfdc12e39868cee6e041fa46a643a16fad
Luigi Improta <luigi@macbookLuigi.local>
2024-06-13 15:49:31 +0200

    gino

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <div class="img"></div>
         <div class="center">
             <div class="title">Vieni a scoprire i nostri film</div>
-            <div>
+            <div class="sub_title" style="font-size: 25px">
                 <h4>Indipendentemente da quale sia il tuo genere preferito o se sei alla ricerca di nuove ispirazioni, <br>
                 Gino's Studios e' qui per soddisfare la tua sete di racconti straordinari. <br>
                  Preparati a lasciarti trasportare dalla magia del cinema con Gino's Studios.</h4>
