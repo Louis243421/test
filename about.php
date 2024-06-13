6ac9f08ac5cc271277046faba943a3734f0d616c
Luigi Improta <luigi@macbookLuigi.local>
2024-06-13 14:31:14 +0200

    gino

--- about.php
+++ about.php
@@ -14,25 +14,25 @@
         <div class="img"></div>
         <div class="center">
             <div class="title"></div>
-            <div id="sub_title">Benvenuti su Gino's Studios
-Scopri un mondo di emozioni e avventure con Gino's Studios, il tuo portale di riferimento per film di alta qualit?. 
-Siamo appassionati di cinema e dediti a offrire una vasta selezione di film che spaziano dai grandi classici ai pi? recenti successi di Hollywood, 
-fino alle gemme indipendenti di tutto il mondo.
+            <div id="sub_title"><h3>Benvenuti su Gino's Studios</h3><br>
+Scopri un mondo di emozioni e avventure con Gino's Studios, il tuo portale di riferimento per film di alta qualit?.<br>
+Siamo appassionati di cinema e dediti a offrire una vasta selezione di film che spaziano dai grandi classici ai pi? recenti successi di Hollywood, <br>
+fino alle gemme indipendenti di tutto il mondo.<br>
 
-<h3>Cosa offriamo:</h3>
-Catalogo Diversificato: Esplora un'ampia gamma di generi, dai thriller mozzafiato alle commedie spassose,
-dai drammi intensi ai film d'animazione per tutta la famiglia.
-Recensioni e Consigli: Leggi recensioni dettagliate e scopri 
-suggerimenti personalizzati per trovare il film perfetto per ogni occasione.
-Novit? e Anteprime: Rimani aggiornato sulle ultime uscite cinematografiche e guarda trailer in anteprima esclusiva.
-Approfondimenti Cinematografici: Immergiti in articoli, interviste
-e reportage che esplorano il dietro le quinte del mondo del cinema.
-La tua esperienza cinematografica inizia qui
-A Gino's Studios, non ci limitiamo a offrirti film: ti offriamo un'esperienza cinematografica completa,
-curata con passione e competenza. Che tu sia un cinefilo esperto o un amante occasionale del cinema, 
-troverai sempre qualcosa che ti appassioner?.
-Unisciti a noi su Gino's Studios e lasciati trasportare dal potere del cinema!
-Questa descrizione mira a trasmettere la passione e la variet? di contenuti che Gino's Studios offre,
+<h3>Cosa offriamo:</h3><br>
+Catalogo Diversificato: Esplora un'ampia gamma di generi, dai thriller mozzafiato alle commedie spassose,<br>
+dai drammi intensi ai film d'animazione per tutta la famiglia.<br>
+Recensioni e Consigli: Leggi recensioni dettagliate e scopri <br>
+suggerimenti personalizzati per trovare il film perfetto per ogni occasione.<br>
+Novit? e Anteprime: Rimani aggiornato sulle ultime uscite cinematografiche e guarda trailer in anteprima esclusiva.<br>
+Approfondimenti Cinematografici: Immergiti in articoli, interviste<br>
+e reportage che esplorano il dietro le quinte del mondo del cinema.<br>
+La tua esperienza cinematografica inizia qui<br>
+A Gino's Studios, non ci limitiamo a offrirti film: ti offriamo un'esperienza cinematografica completa,<br>
+curata con passione e competenza. Che tu sia un cinefilo esperto o un amante occasionale del cinema, <br>
+troverai sempre qualcosa che ti appassioner?.<br>
+Unisciti a noi su Gino's Studios e lasciati trasportare dal potere del cinema!<br>
+Questa descrizione mira a trasmettere la passione e la variet? di contenuti che Gino's Studios offre,<br>
 invitando i visitatori a esplorare e a diventare parte della community.</div>
             <div class="btns">
                 <button>Learn More</button>
