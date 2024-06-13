6bfbb9f454e305183c732d8bdc31ba2ccd545d5e
Luigi Improta <luigi@macbookLuigi.local>
2024-06-13 11:29:03 +0200

    gino

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-echo "ciao";
-?>
+<!DOCTYPE html>
+<!-- Website - www.codingnepalweb.com -->
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gino's Studios</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <?php include 'menu.html'; ?>
+    <div class="img"></div>
+    <div class="center">
+      <div class="title">Vieni a scoprire i nostri film</div>
+      <div class="sub_title"></div>
+      <div class="btns">
+        <button>Learn More</button>
+        <button>Subscribe</button>
+      </div>
+    </div>
+  </body>
+</html>
