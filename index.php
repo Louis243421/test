4d1b3c5adfc7e9d0ab082332273410e6764a4f65
Luigi Improta <luigi@macbookLuigi.local>
2024-06-13 11:35:49 +0200

    gino

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   </head>
   <body>
-    <?php include 'menu.html'; ?>
+    <?php include 'menu.tml'; ?>
     <div class="img"></div>
     <div class="center">
       <div class="title">Vieni a scoprire i nostri film</div>
