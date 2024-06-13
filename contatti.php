e4be54d6ef4bd79a62d91f9001e0f755056e2bcf
Luigi Improta <luigi@macbookLuigi.local>
2024-06-13 15:56:57 +0200

    gino

---
+++ contatti.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gino's Studios</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    
+</head>
+<body>
+    <?php include 'menu.html'; ?>
+    <div class="content">
+        <div class="img"></div>
+        <div class="center">
+            <div class="title"></div>
+            <div id="sub_title"><h3>Contattaci!!!</h3><br>
+            Contatta la nostra redazione al numero: +39 55555555555 <br>
+            Mostra il tuo talento! <br>
+            <h4>Cosa aspetti!!!</h4>
+            </div>
+        </div>
+    </div>
+    <?php include 'footer.html'; ?>
+</body>
+</html>
