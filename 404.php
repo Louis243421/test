09aa9ab082a8a8d6e132dcccf29be8fb33918bb4
Luigi Improta <luigi@macbookLuigi.local>
2024-06-13 17:37:33 +0200

    gino

---
+++ 404.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gino's Studios</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    
+</head>
+<body>
+ <nav>
+    <div class="menu">
+      <div class="logo">
+        <a href="/">Gino's Studios</a>
+      </div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="film.php">Films</a></li>
+        <li><a href="contatti.php">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+    <div class="content">
+        <div class="img"></div>
+        <div class="center">
+            <div class="title">ERROR 404</div>
+            <div class="sub_title" style="font-size: 25px">
+                <h4>Ci dispiace, pagina non trovata! <br></h4>
+                </div>
+            <div class="btns">
+                <button onclick="window.location.href='about.php'">Learn More</button>
+            </div>
+        </div>
+    </div>
+
+	<footer>
+    <p>Created by Improta Luigi&trade;</p>
+  </footer>
+</body>
+</html>
