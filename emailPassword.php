12de5a1283f2f7d47a999278217cf7d2bac63665
Luigi Improta <luigi@macbookLuigi.local>
2024-06-14 17:23:38 +0200

    gino

--- emailPassword.php
+++ emailPassword.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script>
         function inviaDati(event) {
             event.preventDefault(); // Prevenire l'invio predefinito del modulo
