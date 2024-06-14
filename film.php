4bb3d47bed16a43d61925b143e9d59322a6ced97
Luigi Improta <luigi@macbookLuigi.local>
2024-06-14 14:12:26 +0200

    gino

--- film.php
+++ film.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Gino's Studios</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" />
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    
 </head>
 <body>
     <?php include 'menu.html'; ?>
-
+    <div class="content">
+        <div class="img"></div>
+        <div class="center">
+            <div class="title"></div>
+            <div id="sub_title">
+            <?php include 'db.php'; ?> 
             <?php
-                print("<strong>I nostri film:</strong><br>");
+                print("<strong style='font-size: 50px;'>I nostri film:</strong> <br>");
                 ricerca("titolo","film");
-            ?>
-
-
-            <?php
-                print("<strong>I nostri registi:</strong><br>");
+                print("<br>");
+                print("<strong style='font-size: 50px;'>I nostri registi:</strong> <br>");
                 ricerca("nome","registi");
+                print("<strong style='font-size: 50px;'>I nostri attori:</strong> <br>");
+                ricerca("nome","attori"); 
+
+                $conn->close();
             ?>
-
-            <?php
-                print("<strong>I nostri attori:</strong><br>");
-                ricerca("nome","attori");
-            ?>
-
-
+        </div>
+    </div>
     <?php include 'footer.html'; ?>
 </body>
 </html>
