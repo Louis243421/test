4b9dedc4c6262a33762acffea6968fdaf607fb12
Luigi Improta <luigi@macbookLuigi.local>
2024-06-14 16:39:20 +0200

    gino

--- register.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Ottieni il valore dell'email dal modulo
-    $email = $_POST['email'];
-
-    // Fai qualcosa con il valore, ad esempio visualizzarlo
-    echo "L'email inserita ?: " . htmlspecialchars($email);
-}
-?>
