859ce485691934f3cda4989d3ca54d14a2a29748
Luigi Improta <luigi@macbookLuigi.local>
2024-06-14 14:04:20 +0200

    gino

--- film.php
+++ film.php
@@ -1,31 +1,48 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gino's Studios</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    
+    <style>
+        .content {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around; /* Distribuisce lo spazio tra le colonne */
+        }
+        .section {
+            flex: 1 1 300px; /* Flessibilit? per adattarsi al contenuto */
+            margin: 10px;
+            padding: 20px;
+            border: 1px solid #ccc;
+            text-align: center;
+        }
+        .section strong {
+            font-size: 24px;
+        }
+    </style>
 </head>
 <body>
     <?php include 'menu.html'; ?>
     <div class="content">
-        <div class="img"></div>
-        <div class="center">
-            <div class="title"></div>
-            <div id="sub_title">
-            <?php include 'db.php'; ?> 
-            <?php 
-                print("<strong style='font-size: 50px;'>I nostri film:</strong> <br>");
+        <div class="section">
+            <?php
+                print("<strong>I nostri film:</strong><br>");
                 ricerca("titolo","film");
-                print("<br>");
-                print("<strong style='font-size: 50px;'>I nostri registi:</strong> <br>");
+            ?>
+        </div>
+        <div class="section">
+            <?php
+                print("<strong>I nostri registi:</strong><br>");
                 ricerca("nome","registi");
-                print("<strong style='font-size: 50px;'>I nostri attori:</strong> <br>");
-                ricerca("nome","attori"); 
-
-                $conn->close();
+            ?>
+        </div>
+        <div class="section">
+            <?php
+                print("<strong>I nostri attori:</strong><br>");
+                ricerca("nome","attori");
             ?>
         </div>
     </div>
