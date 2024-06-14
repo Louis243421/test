02b88c36401f373ee36f919d71d871594fabab08
Luigi Improta <luigi@macbookLuigi.local>
2024-06-14 14:09:10 +0200

    gino

--- film.php
+++ film.php
@@ -6,46 +6,28 @@
     <title>Gino's Studios</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <style>
-        .content {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-around; /* Distribuisce lo spazio tra le colonne */
-        }
-        .section {
-            flex: 1 1 300px; /* Flessibilit? per adattarsi al contenuto */
-            margin: 10px;
-            padding: 20px;
-            border: 1px solid #ccc;
-            text-align: center;
-        }
-        .section strong {
-            font-size: 24px;
-        }
-    </style>
 </head>
 <body>
     <?php include 'menu.html'; ?>
-    <div class="content">
-        <div class="section">
+        <div id="film">
             <?php
                 print("<strong>I nostri film:</strong><br>");
                 ricerca("titolo","film");
             ?>
         </div>
-        <div class="section">
+        <div id="registi">
             <?php
                 print("<strong>I nostri registi:</strong><br>");
                 ricerca("nome","registi");
             ?>
         </div>
-        <div class="section">
+        <div id="attori">
             <?php
                 print("<strong>I nostri attori:</strong><br>");
                 ricerca("nome","attori");
             ?>
         </div>
-    </div>
+
     <?php include 'footer.html'; ?>
 </body>
 </html>
